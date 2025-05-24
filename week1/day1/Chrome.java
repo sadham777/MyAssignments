6df6c6cae9b4f2d9bb041af93cff9cea449a6b10
sadham777 <sadhamhussain296@gmail.com>
2025-05-24 17:23:17 +0530

    Add files via upload
    Week1

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,31 @@
+package week1.day1;
+
+import org.apache.commons.io.filefilter.FalseFileFilter;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseYear=2008;
+		char shortcutKey='C';
+		
+		System.out.println(version);
+		System.out.println("developer");
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println('C');
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
